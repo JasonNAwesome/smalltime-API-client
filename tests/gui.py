b630cbe10ed81ecc464a701d5f42f5202ec63c60
JasonNAwesome <jasonnova4@gmail.com>
2015-12-28 10:17:07 -0800

    Figuring more stuff out

--- tests/gui.py
+++ tests/gui.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
-import curses
+import curses, sys
 
 screen = curses.initscr()
+curses.start_color()
+
 
 screen.clear
 screen.border(0)
 screen.addstr(3,3,"Choose a choice:")
-screen.addstr(5,5,"A) RA RA RA")
-screen.addstr(6,5,"B) Prooal and alks")
+screen.addstr(5,5,"1) RA RA RA")
+screen.addstr(6,5,"2) Prooal and alks")
 screen.refresh()
 
 n = screen.getch()
+
+if n == ord('1'):
+    screen.clear
+    screen.refresh()
+    screen.border(0)
+    screen.addstr(2,2,"Wow you said RA")
+elif n == ord('2'):
+    screen.clear
+    screen.refresh()
+    screen.border(0)
+    screen.addstr(2,2,"Yes.")
+
+screen.getch()
 
 curses.endwin()
